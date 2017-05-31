a90109f44ffaf2aebc94eac8eea862c5d328f5bd
zhaoerdragon <zhao38276543@126.com>
2017-05-31 19:04:34 +0800

    add GsonUtils file

---
+++ app/src/main/java/com/zhaoerdragon/xuptandroid/tools/GsonUtils.java
@@ -0,0 +1,45 @@
+package com.zhaoerdragon.xuptandroid.tools;
+
+
+import com.google.gson.Gson;
+
+/**
+ * Gson解析工具类
+ */
+public class GsonUtils {
+
+    /**
+     * 工具类不能被初始化
+     */
+    public GsonUtils(){
+        throw new UnsupportedOperationException("cannot be instananed");
+    }
+
+    /**
+     * 解析一个Json数据
+     * @param jsonString
+     * @param tClass
+     * @param <T>
+     * @return
+     */
+    public static <T> T getInfo(String jsonString ,Class<T> tClass){
+        T t = null;
+        Gson gson =new Gson();
+        t = gson.fromJson(jsonString,tClass);
+        return t;
+    }
+
+    /**
+     * 解析一组Json，转换为数组形式的数据
+     * @param jsonString
+     * @param tClass
+     * @param <T>
+     * @return
+     */
+    public static <T> T[] getArrayInfo(String jsonString, Class<T[]> tClass) {
+        Gson gson = new Gson();
+        T[] t = gson.fromJson(jsonString, tClass);
+
+        return t;
+    }
+}
--- app/src/main/java/com/zhaoerdragon/xuptandroid/tools/LogUtils.java
+++ app/src/main/java/com/zhaoerdragon/xuptandroid/tools/LogUtils.java
@@ -7,7 +7,14 @@ import android.util.Log;
  */
 public class LogUtils {
 
+    /**
+     * 测试标志，应用发布时可以将值设置为false，则不会打印出Log
+     */
     public static boolean isDebug = true;
+
+    /**
+     * 打印标志
+     */
     private static final String TAG = "hahaha";
 
     /**
