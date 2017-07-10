d886614d7ea977a2b597b4238b83d044f9ff93b7
zhaoerdragon <zhao38276543@126.com>
2017-07-10 16:41:59 +0800

    更新类型安全转换函数

--- app/src/main/java/com/zhaoerdragon/xuptandroid/MainActivity.java
+++ app/src/main/java/com/zhaoerdragon/xuptandroid/MainActivity.java
@@ -3,16 +3,18 @@ package com.zhaoerdragon.xuptandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zhaoerdragon.xuptandroid.tools.ConvertSafeUtils;
+import com.zhaoerdragon.xuptandroid.tools.LogUtils;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
     }
 }
